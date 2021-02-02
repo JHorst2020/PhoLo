ff7e2e4226a85fc83f6ba54a6252371090a9f93b
Jerzy <jerzyhorst@gmail.com>
2021-02-01 18:50:54 -0800

    first commit

---
+++ frontend/src/store/session.js
@@ -0,0 +1,82 @@
+import { fetch } from "../util/csrf.js";
+
+const SET_USER = "session/setUser";
+const REMOVE_USER = "session/removeUser";
+
+const setUser = (user) => ({
+  type: SET_USER,
+  payload: user,
+});
+
+const removeUser = () => ({
+  type: REMOVE_USER,
+});
+
+export const login = ({ credential, password }) => async (dispatch) => {
+  const res = await fetch("/api/session", {
+    method: "POST",
+    body: JSON.stringify({ credential, password }),
+  });
+  dispatch(setUser(res.data.user));
+};
+
+export const restoreUser = () => async (dispatch) => {
+  const res = await fetch("/api/session");
+  dispatch(setUser(res.data.user));
+};
+
+export const createUser = (user) => async (dispatch) => {
+  const { images, image, username, email, password } = user;
+  const formData = new FormData();
+  formData.append("username", username);
+  formData.append("email", email);
+  formData.append("password", password);
+
+  // for multiple files
+  if (images && images.length !== 0) {
+    for (var i = 0; i < images.length; i++) {
+      formData.append("images", images[i]);
+    }
+  }
+
+  // for single file
+  if (image) formData.append("image", image);
+
+  const res = await fetch(`/api/users/`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "multipart/form-data",
+    },
+    body: formData,
+  });
+
+  dispatch(setUser(res.data.user));
+};
+
+export const logout = () => async (dispatch) => {
+  const res = await fetch("/api/session", {
+    method: "DELETE",
+  });
+
+  if (res.data.message === "success") {
+    dispatch(removeUser());
+  }
+};
+
+const initialState = { user: null };
+
+function reducer(state = initialState, action) {
+  let newState;
+  switch (action.type) {
+    case SET_USER:
+      // I prefer this syntax rather than the Object.assign(...)
+      return { ...state, user: action.payload };
+    case REMOVE_USER:
+      newState = Object.assign({}, state, { user: null });
+      return newState;
+    default:
+      return state;
+  }
+}
+
+export default reducer;
