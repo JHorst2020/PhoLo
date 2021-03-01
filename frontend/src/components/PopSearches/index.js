3af9115974a621f5bbcf2d791fca41c4c92e3048
Jerzy <jerzyhorst@gmail.com>
2021-02-28 17:29:21 -0800

    changed the quick search entries

--- frontend/src/components/PopSearches/index.js
+++ frontend/src/components/PopSearches/index.js
@@ -30,7 +30,7 @@ export default function PopSearches(){
             }))
     }
 return (
-    <div style={{display:"flex", flexDirection:"column", backgroundColor:"#f8f8ff", width:"100vw"}}>
+    <div className="popularSearches" style={{display:"flex", flexDirection:"column", backgroundColor:"#f8f8ff", width:"100vw"}}>
         <h2>Popular Searches</h2>
     <div style={{display:"flex"}}>
         <div style={{display:"flex", flexDirection:"column", marginLeft:"20px"}}>
@@ -40,24 +40,20 @@ return (
         </div>
         <div style={{display:"flex", flexDirection:"column", marginLeft:"20px"}}>
             <h3>Las Vegas</h3>
-            <Button color="primary" onClick={() => vintageLasVegas(["1940-01-01", "1975-01-01"],"Las Vegas")}>1940 to 1975</Button>
-            <Button color="primary" onClick={() => vintageLasVegas(["1975-01-01", "2020-12-31"],"Las Vegas")}>1975 to 2020</Button>
+            <Button color="primary" onClick={() => vintageLasVegas(["1940-01-01", "1975-01-01"],"Las Vegas")}>1940 - 1975</Button>
+            <Button color="primary" onClick={() => vintageLasVegas(["1975-01-01", "2020-12-31"],"Las Vegas")}>1975 - 2020</Button>
         </div>
-        <div style={{display:"flex", flexDirection:"column", marginLeft:"20px"}}>
+        {/* <div style={{display:"flex", flexDirection:"column", marginLeft:"20px"}}>
             <h3>Tahrir Square</h3>
             <Button color="primary" onClick={() => vintageLasVegas(["1965-01-01", "1975-01-01"],"Tahrir Square, Baghdad, Iraq")}>1965 to 1975</Button>
             <Button color="primary" onClick={() => vintageLasVegas(["2019-01-01", "2021-01-01"],"Tahrir Square, Baghdad, Iraq")}>2019 to 2021</Button>
-        </div>
+        </div> */}
         <div style={{display:"flex", flexDirection:"column", marginLeft:"20px"}}>
             <h3>Disneyland</h3>
-            <Button color="primary" onClick={() => vintageLasVegas(["1955-01-01", "1970-01-01"],"Disneyland")}>1955 to 1970</Button>
-            <Button color="primary" onClick={() => vintageLasVegas(["2019-01-01", "2021-01-01"],"Disneyland")}>2019 to 2021</Button>
+            <Button color="primary" onClick={() => vintageLasVegas(["1955-01-01", "1970-01-01"],"Disneyland")}>1955 - 1970</Button>
+            <Button color="primary" onClick={() => vintageLasVegas(["2019-01-01", "2021-01-01"],"Disneyland")}>2019 - 2021</Button>
         </div>
-        <div style={{display:"flex", flexDirection:"column", marginLeft:"20px"}}>
-            <h3>Fiji</h3>
-            <Button color="primary" onClick={() => vintageLasVegas(["2019-08-01", "2019-08-30"],"Denarau Island South, Denarau Island, Fiji")}>Aug 2019</Button>
-            {/* <Button color="primary" onClick={() => vintageLasVegas(["1999-01-01", "2070-01-01"],"amsterdam")}>1949 to 1970</Button> */}
-        </div>
+        
         
     </div>
     </div>
